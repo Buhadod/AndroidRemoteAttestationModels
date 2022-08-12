a32ec09c69ebcadfa38f24970053696eab2719e8
akaldoseri <buhadod@gmail.com>
2022-08-13 00:13:38 +0300

    clean up

--- run.bat
+++ run.bat
@@ -1,1 +1,3 @@
 docker exec -it relaxed_sammet tamarin-prover interactive  /root/tamarin/CodeProtectionModel/sapic/.  --interface=*4 -p=8200
+
+tamarin-prover interactive CodeProtectionModel/sapic/. --interface=*4 -p=8200
--- sapic/completed_proofs/Readme.txt
+++
@@ -1,9 +0,0 @@
-Missing proofs:
-
-Framework_KeyAttestation_App_NoNonce_INCOMPLETE:
-- lemma CertPossible
-- lemma CertFresh
-- lemma Correctness_Both_Valid
-- lemma Correctness_Rooting_Device
-
-
