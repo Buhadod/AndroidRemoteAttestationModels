0228454908ae020199027c80991a74f77a6133f0
akaldoseri <buhadod@gmail.com>
2022-08-22 22:25:48 +0300

    code protection 3 works

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 docker exec -it relaxed_sammet tamarin-prover interactive  /root/tamarin/CodeProtectionModel/sapic/.  --interface=*4 -p=8200
 
 tamarin-prover interactive . --interface=*4 -p=8200
+tamarin-prover <filename> --prove=<lemma name>
